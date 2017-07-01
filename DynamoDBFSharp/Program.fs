f98dc91a5024b92e4e82d79a12b2672c0cb251eb
Jonathan Nolis <jnolis@noreply.users.github.com>
2017-07-01 11:13:52 -0700

    Initial commit

---
+++ DynamoDBFSharp/Program.fs
@@ -0,0 +1,29 @@
+﻿
+open System
+open System.Collections.Generic
+
+open Amazon.DynamoDBv2
+open Amazon.DynamoDBv2.DocumentModel
+open Amazon.DynamoDBv2.Model;
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+
+
+[<EntryPoint>]
+let main argv = 
+    let client = new AmazonDynamoDBClient()
+    client.CreateTable( 
+        new CreateTableRequest(
+                    TableName = "Businesses",
+                    ProvisionedThroughput = new ProvisionedThroughput(ReadCapacityUnits = 3L, WriteCapacityUnits = 1L),
+                    KeySchema = List [new KeySchemaElement(AttributeName = "Name", KeyType = KeyType.HASH);
+                                      new KeySchemaElement(AttributeName = "Id",KeyType = KeyType.RANGE)],
+                    AttributeDefinitions = 
+                        List [
+                                new AttributeDefinition(AttributeName = "Name", AttributeType = ScalarAttributeType.S);
+                                new AttributeDefinition(AttributeName = "Id", AttributeType = ScalarAttributeType.N)]
+                ))
+    |> ignore
+    printfn "%A" argv
+    0 // return an integer exit code
